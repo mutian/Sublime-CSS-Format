de4daaf2cbf9cee2e8eae81da8abbf4489ea3752
mutian <mutian@me.com>
2014-01-28 00:40:13 +0800

    Improve: Format of comment.

--- css_format.py
+++ css_format.py
@@ -41,16 +41,20 @@ class CssFormatCommand(sublime_plugin.TextCommand):
 			'compress'	: self.compress_rules
 		}
 		code = re.sub(r"\s*([\{\}:;,])\s*", r"\1", code)		# remove \s before and after characters {}:;,
-		code = re.sub(r",[\s\.\#\d]*\{", "{", code)				# remove invalid selector
+		code = re.sub(r",[\d\s\.\#\+>:]*\{", "{", code)			# remove invalid selector
 		code = re.sub(r";\s*;", ";", code)						# remove superfluous ;
 
 		if action != 'compress':
+			code = re.sub(r"\/\*\s*([\s\S]+?)\s*\*\/", r"/* \1 */", code)	# add space before and after comment content
+			code = re.sub(r"\}\s*(\/\*[\s\S]+?\*\/)\s*", r"}\n\1\n", code)	# add \n before and after outside comment
 			code = re.sub(r",(\S)", r", \1", code)							# add space after ,
 			code = re.sub(r"([A-Za-z-]):([^;\{]+[;\}])", r"\1: \2", code)	# add space after properties' :
 			code = re.sub(r"(http[s]?:) \/\/", r"\1//", code)				# fix space after http[s]:
 			code = re.sub(r"\s*!important", r" !important", code)			# add space before !important
 
 		code = actions[action](code)
+
+		code = re.sub(r"(@import[^;]+;)\s*", r"\1\n", code)		# add \n and remove \t after @import
 		code = re.sub(r"^\s*(\S+(\s+\S+)*)\s*$", r"\1", code)	# remove superfluous \s
 		return code
 
@@ -62,7 +66,6 @@ class CssFormatCommand(sublime_plugin.TextCommand):
 		code = re.sub(r"(\S);([^\}])", r"\1; \2", code)			# add space after ;
 		code = re.sub(r"(\S)\}", r"\1 }", code)					# add space before }
 		code = re.sub(r"\}", r"}\n", code)						# add \n after }
-		code = re.sub(r"(@import[^;]+;)\s*", r"\1\n", code)		# add \n after @import
 		#code = re.sub(r"\{([^\{\}])*\}", compact_rules_brace_block, code)	# todo:
 		return code
 
@@ -77,13 +80,13 @@ class CssFormatCommand(sublime_plugin.TextCommand):
 
 		code = re.sub(r"(\S)\{(\S)", r"\1 {\n\t\2", code)				# add space before { , and add \n\t after {
 		code = re.sub(r"(\S);([^\}])", r"\1;\n\t\2", code)				# add \n\t after ;
-		code = re.sub(r"([^\}])\}", r"\1\n}", code)						# add \n before }
+		code = re.sub(r"\;\s*(\/\*[^\n]*\*\/)\s*", r"; \1\n\t", code)	# fix comment after ;
+		code = re.sub(r"([^\}])\s*\}", r"\1\n}", code)					# add \n before }
 		code = re.sub(r"\}", r"}\n", code)								# add \n after }
-		code = re.sub(r"(@import[^;]+;)\s*", r"\1\n", code)				# remove \t after @import
 		return code
 	
 	def compress_rules(self, code):
-		code = re.sub(r"\/\*(.|\n)*?\*\/", "", code)		# remove comments
+		code = re.sub(r"\/\*[\s\S]+?\*\/", "", code)		# remove non-empty comments, /**/ maybe a hack
 		code = re.sub(r"\s*([\{\}:;,])\s*", r"\1", code)	# remove \s before and after characters {}:;, again
 		code = re.sub(r"\s*(!important)", r"\1", code)		# remove space before !important
 		return code
