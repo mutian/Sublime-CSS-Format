a78d3190844ff6d9d995932d4b2202103f4c2326
mutian <mutian@me.com>
2015-02-26 23:41:29 +0800

    V1.0.5
    1. Protect strings in properties.
    2. Optimize comments' format.
    3. Optimize commas' format.

--- libs/cssformatter.py
+++ libs/cssformatter.py
@@ -1,4 +1,5 @@
-#coding=utf-8
+#!/usr/bin/python
+# encoding: utf-8
 #
 # Convert CSS/SASS/SCSS/LESS code to Expanded, Compact or Compressed format.
 #
@@ -26,30 +27,40 @@ def format_code(code, action='compact', indentation='\t'):
 	# Comments
 	if action == 'compress':
 		# remove comments
-		code = re.sub(r'\/\*[\s\S]*?\*\/', '', code)
+		code = re.sub(r'\s*\/\*[\s\S]*?\*\/\s*', '', code)
 	else:
 		# Protect Comments
-		comments = re.findall(r'[ \t]*\/\*[\s\S]*?\*\/', code)
-		code = re.sub(r'[ \t]*\/\*[\s\S]*?\*\/', '</**/>', code)
+		commentReg = r'[ \t]*\/\*[\s\S]*?\*\/'
+		comments = re.findall(commentReg, code)
+		code = re.sub(commentReg, '!comment!', code)
+
+	# Protect Strings
+	stringReg = r'(content\s*:|[\w-]+\s*=)\s*(([\'\"]).*?\3)\s*'
+	strings = re.findall(stringReg, code)
+	code = re.sub(stringReg, r'\1!string!', code)
 
 	# Protect Urls
-	urls = re.findall(r'url\([^\)]+\)', code)
-	code = re.sub(r'url\([^\)]+\)', '<url()>', code)
+	urlReg = r'((?:url|url-prefix|regexp)\([^\)]+\))'
+	urls = re.findall(urlReg, code)
+	code = re.sub(urlReg, '!url!', code)
 
 	# Pre Process
 	code = re.sub(r'\s*([\{\}:;,])\s*', r'\1', code)	# remove \s before and after characters {}:;,
-	code = re.sub(r',[\d\s\.\#\+>:]*\{', '{', code)		# remove invalid selectors without \w
+	code = re.sub(r'([\[\(])\s*', r'\1', code)			# remove space inner [ or (
+	code = re.sub(r'\s*([\)\]])', r'\1', code)			# remove space inner ) or ]
+	# code = re.sub(r'(\S+)\s*([\+>~])\s*(\S+)', r'\1\2\3', code)	# remove \s before and after relationship selectors
+	code = re.sub(r',[\d\s\.\#\+>~:]*\{', '{', code)	# remove invalid selectors without \w
 	code = re.sub(r'([;,])\1+', r'\1', code)			# remove repeated ;,
 
 	if action != 'compress':
-		# selectors group
-		if re.search(r'-bs', action):
+		# group selector
+		if re.search('-bs', action):
 			code = break_selectors(code)				# break after selectors' ,
 		else:
-			code = re.sub(r',(\S)', r', \1', code)		# add space after ,
+			code = re.sub(r',\s*', ', ', code)			# add space after ,
 
-		# add space after :
-		if re.search(r'-ns', action):
+		# add space
+		if re.search('-ns', action):
 			code = re.sub(r', +', ',', code)								# remove space after ,
 			code = re.sub(r'\s+!important', '!important', code)				# remove space before !important
 		else:
@@ -62,23 +73,27 @@ def format_code(code, action='compact', indentation='\t'):
 
 	if action == 'compress':
 		# remove last semicolon
-		code = re.sub(r';\}', r'}', code)
+		code = code.replace(';}', '}')
 	else:
 		# Fix Comments
-		code = re.sub(r'\s*<\/\*\*\/>\s*@', r'\n\n</**/>\n@', code)
-		code = re.sub(r'\s*<\/\*\*\/>\s*([^\/\{\};]+?){', r'\n\n</**/>\n\1{', code)
-		code = re.sub(r'\s*\n<\/\*\*\/>', r'\n\n</**/>', code)
+		code = re.sub(r'\s*!comment!\s*@', '\n\n!comment!\n@', code)
+		code = re.sub(r'\s*!comment!\s*([^\/\{\};]+?){', r'\n\n!comment!\n\1{', code)
+		code = re.sub(r'\s*\n!comment!', '\n\n!comment!', code)
 
 		# Backfill Comments
 		for i in range(len(comments)):
-			code = re.sub(r'[ \t]*<\/\*\*\/>', comments[i], code, 1)
+			code = re.sub(r'[ \t]*!comment!', comments[i], code, 1)
 
 		# Indent
 		code = indent_code(code, indentation)
 
+	# Backfill Strings
+	for i in range(len(strings)):
+		code = code.replace('!string!', strings[i][1], 1)
+
 	# Backfill Urls
 	for i in range(len(urls)):
-		code = re.sub(r'<url\(\)>', urls[i], code, 1)
+		code = code.replace('!url!', urls[i], 1)
 
 	# Trim
 	code = re.sub(r'^\s*(\S+(\s+\S+)*)\s*$', r'\1', code)
@@ -88,35 +103,35 @@ def format_code(code, action='compact', indentation='\t'):
 
 # Expand Rules
 def expand_rules(code):
-	code = re.sub(r'\{', r' {\n', code)									# add space before { and add \n after {
+	code = re.sub('{', ' {\n', code)									# add space before { and add \n after {
 
-	code = re.sub(r';', r';\n', code)									# add \n after ;
+	code = re.sub(';', ';\n', code)										# add \n after ;
 	code = re.sub(r';\s*([^\{\};]+?){', r';\n\n\1{', code)				# double \n between ; and include selector
 
-	code = re.sub(r'\s*(<\/\*\*\/>)\s*;\s*', r' \1 ;\n', code)			# fix comment before ;
-	code = re.sub(r'(:[^:;]+;)\s*(<\/\*\*\/>)\s*', r'\1 \2\n', code)	# fix comment after ;
+	code = re.sub(r'\s*(!comment!)\s*;\s*', r' \1 ;\n', code)			# fix comment before ;
+	code = re.sub(r'(:[^:;]+;)\s*(!comment!)\s*', r'\1 \2\n', code)		# fix comment after ;
 
-	code = re.sub(r'\s*\}', r'\n}', code)								# add \n before }
-	code = re.sub(r'\}\s*', r'}\n', code)								# add \n after }
+	code = re.sub(r'\s*\}', '\n}', code)								# add \n before }
+	code = re.sub(r'\}\s*', '}\n', code)								# add \n after }
 
 	return code
 
 
 # Compact Rules
 def compact_rules(code):
-	code = re.sub(r'\{', r' { ', code)										# add space and after {
+	code = re.sub('{', ' { ', code)											# add space before and after {
 	code = re.sub(r'(@[\w-]*(document|font-feature-values|keyframes|media|supports)[^;]*?\{)\s*', r'\1\n', code)
 																			# add \n after @xxx {
 
-	code = re.sub(r';', r'; ', code)										# add \n after ;
+	code = re.sub(';', '; ', code)											# add space after ;
 	code = re.sub(r'(@(charset|import|namespace).+?;)\s*', r'\1\n', code)	# add \n after @charset & @import
 	code = re.sub(r';\s*([^\};]+?\{)', r';\n\1', code)						# add \n before included selector
 
-	code = re.sub(r'\s*(<\/\*\*\/>)\s*;', r' \1 ;', code)					# fix comment before ;
-	code = re.sub(r'(:[^:;]+;)\s*(<\/\*\*\/>)\s*', r'\1 \2 ', code)			# fix comment after ;
+	code = re.sub(r'\s*(!comment!)\s*;', r' \1 ;', code)					# fix comment before ;
+	code = re.sub(r'(:[^:;]+;)\s*(!comment!)\s*', r'\1 \2 ', code)			# fix comment after ;
 
-	code = re.sub(r'\s*\}', r' }', code)									# add space before }
-	code = re.sub(r'\}\s*', r'}\n', code)									# add \n after }
+	code = re.sub(r'\s*\}', ' }', code)										# add space before }
+	code = re.sub(r'\}\s*', '}\n', code)									# add \n after }
 
 	return code
 
@@ -129,10 +144,10 @@ def compact_ns_rules(code):
 	code = re.sub(r'(@(charset|import|namespace).+?;)\s*', r'\1\n', code)	# add \n after @charset & @import
 	code = re.sub(r';\s*([^\};]+?\{)', r';\n\1', code)						# add \n before included selector
 
-	code = re.sub(r'\s*(<\/\*\*\/>)\s*;', r'\1;', code)						# fix comment before ;
-	code = re.sub(r'(:[^:;]+;)\s*(<\/\*\*\/>)\s*', r'\1\2', code)			# fix comment after ;
+	code = re.sub(r'\s*(!comment!)\s*;', r'\1;', code)						# fix comment before ;
+	code = re.sub(r'(:[^:;]+;)\s*(!comment!)\s*', r'\1\2', code)			# fix comment after ;
 
-	code = re.sub(r'\}\s*', r'}\n', code)									# add \n after }
+	code = re.sub(r'\}\s*', '}\n', code)									# add \n after }
 
 	return code
 
@@ -149,21 +164,29 @@ def compress_rules(code):
 # Break after Selector
 def break_selectors(code):
 	block = code.split('}')
-
 	for i in range(len(block)):
+
 		b = block[i].split('{')
-		for j in range(len(b)):
-			if b[j].count('@import'):
-				s = b[j].split(';')
-				for k in range(len(s)):
-					if not s[k].count('@import'):					# ignore @import
-						s[k] = re.sub(r',(\S)', r',\n\1', s[k])
-				b[j] = ';'.join(s)
+		bLen = len(b)
+		for j in range(bLen):
+
+			if j == bLen - 1:
+				b[j] = re.sub(r',\s*', ', ', b[j])			# add space after properties' ,
 			else:
-				if j == len(b) - 1 or b[j].count('@media'):
-					b[j] = re.sub(r',(\S)', r', \1', b[j])			# add space after properties' or @media's ,
+				s = b[j].split(';')
+				sLen = len(s)
+				sLast = s[sLen - 1]
+
+				for k in range(sLen - 1):
+					s[k] = re.sub(r',\s*', ', ', s[k])		# add space after properties' ,
+
+				if re.match(r'\s*@(document|media)', sLast):
+					s[sLen - 1] = re.sub(r',\s*', ', ', sLast)		# add space after @media's ,
 				else:
-					b[j] = re.sub(r',(\S)', r',\n\1', b[j])			# add \n after selectors' ,
+					s[sLen - 1] = re.sub(r',\s*', ',\n', sLast)		# add \n after selectors' ,
+
+				b[j] = ';'.join(s)
+
 		block[i] = '{'.join(b)
 
 	code = '}'.join(block)
